57afd5ebd78f8934abc7b59e468015f1934ab50d
Orhan Odabaşı <0rh.odabasi@gmail.com>
2018-11-29 13:13:08 +0300

    Araç modülünde değişiklik yapıldı.

--- Arac/models.py
+++ Arac/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from Musteri.models import Saha
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
 class Arac(models.Model):
@@ -11,10 +13,10 @@ class Arac(models.Model):
 
     arac_plaka = models.CharField(max_length=11, null=False, blank=False, unique=True, verbose_name="Araç Plakası")
     arac_model = models.CharField(max_length=25, null=False, blank=False, verbose_name="Araç Modeli")
-    arac_sigorta_tarih = models.DateField(null=True, blank=True, verbose_name="Araç Sigorta Tarihi")
-    arac_muayene_tarih = models.DateField(null=True, blank=True, verbose_name="Araç Muayene Tarihi")
+    arac_sigorta_tarih = models.DateField(null=True, blank=True, verbose_name="Araç Sigorta Bitiş Tarihi")
+    arac_muayene_tarih = models.DateField(null=True, blank=True, verbose_name="Araç Muayene Bitiş Tarihi")
     arac_bakim_tarih = models.DateField(null=True, blank=True, verbose_name="Araç Bakım Tarihi")
-    arac_ego_tarih = models.DateField(null=True, blank=True, verbose_name="Araç Ego Tarihi")
+    arac_ego_tarih = models.DateField(null=True, blank=True, verbose_name="Araç Ego Bitiş Tarihi")
 
     def __str__(self):
         return f"{self.arac_plaka} - {self.arac_model}"
@@ -34,9 +36,11 @@ class AracKullanim(models.Model):
 
     arac_surucu = models.ForeignKey(User, null=False, blank=False, on_delete=models.CASCADE, verbose_name="Araç Sürücüsü")
     arac_kullanim = models.ForeignKey(Arac, default=0, null=False, blank=False, on_delete=models.CASCADE, verbose_name="Kullandığı Araç") # fix default=0
+    # aracın gittiği müşteri / saha
+    arac_saha_dest = models.ForeignKey(Saha, default=0, null=False, blank=False, on_delete=models.CASCADE, verbose_name="Aracın Gittiği Saha")
     arac_alis_tarih = models.DateTimeField(null=False, blank=False, verbose_name="Araç Alış Tarihi")
     arac_teslim_tarihi = models.DateTimeField(null=False, blank=False, verbose_name="Araç Teslim Tarihi")
-    arac_teslim_km = models.IntegerField(null=False, blank=False, verbose_name="Teslim KM")
+    arac_teslim_km = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(500000)], null=False, blank=False, verbose_name="Teslim KM")
 
     def __str__(self):
-        return f"{self.arac_surucu} || {self.arac_teslim_km}"
+        return f"{self.arac_surucu} - {self.arac_saha_dest} - {self.arac_teslim_km} km"
